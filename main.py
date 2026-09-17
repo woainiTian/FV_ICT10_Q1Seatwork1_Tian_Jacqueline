185660537d67d841ce461b325d9d4282656214b8
jacquetian <tian.jd@obmontessori.edu.ph>
2026-09-17 20:27:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Seatwork 1
-from pyscript import display
+from pyscript import display, document
 
 #Personal Info
 
@@ -9,7 +9,7 @@ name = 'Jacqueline Tian'  #string
 age = 15 #integer
 height1 = 160 #integer
 my_countries = ['Switzerland', 'Italy', 'France'] #list
-student_type = False
+student_type = False #Boolean
 dct_color = {'fav_color':'yellow',}
 dct_car = {'car_brand':'Toyota',}
 dct_shoe ={'shoe_size':'7',}
@@ -17,17 +17,17 @@ dct_bsf = {'best_friend':'Julia',}
 fruits = set(['Mango','Mangosteen','Orange','Pomelo', 'Avocado']) #set
 days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') #tuple
 
-#join the list to remove special characters when displayed
-# mc1 = f" I want to visit {"".join(my_countries)}
+#join the list to remove special characters when displayed by:{", ".join(listvariable)}
 
 
-display (f'Hello! Im {name}! Today, I am {age} years old.', target= "content")
-display (f'I am {height1}cm tall', target="content")
-display (f'{mc1}', target="content")
-display (f'I am a new student...aaand that is {student_type}', target="content")
-display (f'My favorite color as of now is {dct_color}, the car I have is a {dct_car}, and my best friend is {dct_bsf}', target="content")
-display (f'My favorite fruits are {fruits}', target="content")
-display (f'The seven days of the week are: {days}', target="content")
+
+display (f'Hello! Im {name}! I am {age} years old.', target= "content")
+
+display (f'I am {height1}cm tall.', target="content")
+display (f'I am a new student. (That that is {student_type}).', target="content")
+display (f'My favorite color as of now is {dct_color["fav_color"]}, the car I have is a {dct_car["car_brand"]}, and my best friend is {dct_bsf["best_friend"]}.', target="content")
+display (f'My favorite fruits are {", ".join(fruits)}', target="content")
+display (f'The seven days of the week are: {", ".join(days)}.', target="content")
 
 
 #Operations
@@ -35,13 +35,39 @@ display (f'The seven days of the week are: {days}', target="content")
     #declare variable
 
 #for button pyclick
-def compute(e):
-    document.getElementById("results").innerHTML = ""
 
-value1 = float(document.getElementById("value1").value)
-value2 = float(document.getElementById("value2").value)
-result = value1 + value2
-display(result, target="results")
+def add(e):
+    document.getElementById("output").innerHTML = ""
+    value1 = float(document.getElementById("value1").value)
+    value2 = float(document.getElementById("value2").value)
+    added = value1 + value2
+    display (added, target="output")
+
+def subtract(e):
+    document.getElementById("output").innerHTML = ""
+    value1 = float(document.getElementById("value1").value)
+    value2 = float(document.getElementById("value2").value)
+    subtracted = value1 - value2
+    display (subtracted, target="output")
+
+def multiply(e):
+    document.getElementById("output").innerHTML = ""
+    value1 = float(document.getElementById("value1").value)
+    value2 = float(document.getElementById("value2").value)
+    multiplied = value1 * value2
+    display (multiplied, target="output")
+
+def divide(e):
+    document.getElementById("output").innerHTML = ""
+    value1 = float(document.getElementById("value1").value)
+    value2 = float(document.getElementById("value2").value)
+    divided = value1 / value2
+    display (divided, target="output")
+
+
+
+
+
 
 
 
